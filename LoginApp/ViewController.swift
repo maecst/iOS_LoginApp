9bb63d1412ae8c150563c741b122b1d6210b7d66
Mae Yee <Mae@Housam-Halliss-MacBook-Pro.local>
2015-02-10 18:21:07 -0800

    Updates

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -8,11 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        usernameField.delegate = self
+        passwordField.delegate = self
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) ->Bool {
+        let nextTag = textField.tag + 1 as Int
+        var nextField: UIResponder? = textField.superview?.viewWithTag(nextTag)
+        
+        if let field: UIResponder = nextField {
+            field.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+        }
+        
+        return false
     }
 
     override func didReceiveMemoryWarning() {
